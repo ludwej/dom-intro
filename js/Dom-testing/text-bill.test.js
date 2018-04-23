bf03c0095dcd5c0f793031517636a5fae42964c1
ludwej <jacobsludwe84@gmail.com>
2018-04-23 15:27:30 +0200

    testing

--- js/Dom-testing/text-bill.test.js
+++ js/Dom-testing/text-bill.test.js
@@ -1,12 +1,1 @@
-describe('Calculate Bill' , function(){
-    it('should total amount of calls' , function(){
-        assert.equal(addBtn('call,call'),5.50)
 
-});
-  it('should total amount of calls and smses' , function(){
-    assert.equal(addBtn('call,call,sms,sms,call'),9.75)
-});
-it('should total amount of smses' , function(){
-    assert.equal(addBtn('sms,sms,sms,sms'),3.00)
-});
-});
